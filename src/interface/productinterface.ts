96216c93d4c6a5b4c268ab7387d75ae9bbcf4c7c
joshua <jeyalo781227@gmail.com>
2024-07-19 14:58:05 +0200

    aggiornamentisito

--- src/interface/productinterface.ts
+++ src/interface/productinterface.ts
@@ -1,3 +1,5 @@
+import { url } from "inspector";
+
 export interface Product {
     id: string;
     name: string;
@@ -6,49 +8,72 @@ export interface Product {
     description: string;
 }
  const mockProducts: Product[] =  [
-    {
+{
     id: "1",
-    name: "Davide il capo a cavallo",
-    image: "https://th.bing.com/th/id/OIG4.9.BBCAIEJ_B35yDymd9_?pid=ImgGn",
-    price: "1.000.000,00€",
-    description: "Uomo dall'immensa bellezza che vive la vita nel migliore dei modi, affrontando tutte le difficoltà insieme al suo prode cavallo Pippo.",
+    name: "Fanta Grape Soda",
+    image: "https://www.jerryamerica.com/cdn/shop/products/fead87c4-4d8b-4f51-af89-f6229d54f70d_360x.jpg?v=1600294070",
+    price: "2,99€",
+    description: " Fanta gusto Uva per la volpa che non arriva all'uva.",
 
 },
 {
     id: "2",
-    name: "Joshua e la sua papera",
-    image: "https://th.bing.com/th/id/OIG3.wwy4I8dRwQUeoCJNbwGe?pid=ImgGn",
-    price: "0,99€",
-    description: "Solo Joshua con l'ossessione per le papere di gomma.",
+    name: "Fanta Strawberry Soda",
+    image: "https://www.jerryamerica.com/cdn/shop/products/FANTA-STRAWBERRY-SODA-FRAGOLA_360x.jpg?v=1571687584",
+    price: "2,99€",
+    description: "Fanta gusto fragola.",
 
 },{
     id: "3",
-    name: "Vincenzo che psicanalizza un pinguino",
-    image: "https://th.bing.com/th/id/OIG4.8qSan3.NEFrfg9hJ0nQj?pid=ImgGn",
-    price: "Un rene €",
-    description: "Lo psicologo Vincenzo analizza il pinguino depresso rifiugiato in Italia per colpa del riscaldamento globale",
+    name: "Fanta Berry Soda",
+    image: "https://www.jerryamerica.com/cdn/shop/products/fanta-berry-soda_360x.jpg?v=1571687576",
+    price: "2,99 €",
+    description: "Fanta gusto bacche",
 
 },{
     id: "4",
-    name: "Lucia e i gatti",
-    image: "https://th.bing.com/th/id/OIG1.F89.D4o6EKHkGTBDxhpX?pid=ImgGn",
-    price: "un paio di croccantini e un pesciolino €",
-    description: "Dopo innumerevoli tentativi nel creare un'opera d'arte che possa rappresentare la nostra Lucia ci siamo dovuti accontentare di questa foto con i gatti... che carini i gattini ",
+    name: "Samyan Extreme Spicy Chips",
+    image: "https://www.jerryamerica.com/cdn/shop/files/Samyang-Extreme-Buldak-Zzaldduk-2xSpicy-Chips-80g-550x550_360x.jpg?v=1706881846",
+    price: "3,99 €",
+    description: "Patatina estremamente piccanti, per sicurezza compra un po' di latte ",
 
 },
 {
-    id: "3",
-    name: "Vincenzo che psicanalizza un pinguino",
-    image: "https://th.bing.com/th/id/OIG4.8qSan3.NEFrfg9hJ0nQj?pid=ImgGn",
-    price: "Un rene €",
-    description: "Lo psicologo Vincenzo analizza il pinguino depresso rifiugiato in Italia per colpa del riscaldamento globale",
+    id: "5",
+    name: "Lays Chips",
+    image: "https://www.jerryamerica.com/cdn/shop/files/lays-kfc-chips-150g_700x_bc9ce93b-d8ca-4414-8572-be100fc2d376_360x.jpg?v=1697201475",
+    price: "2,99 €",
+    description: "Patatine Lays semplici, infatti non costano molto",
 
 },{
-    id: "4",
-    name: "Lucia e i gatti",
-    image: "https://th.bing.com/th/id/OIG1.F89.D4o6EKHkGTBDxhpX?pid=ImgGn",
-    price: "un paio di croccantini e un pesciolino €",
-    description: "Dopo innumerevoli tentativi nel creare un'opera d'arte che possa rappresentare la nostra Lucia ci siamo dovuti accontentare di questa foto con i gatti... che carini i gattini ",
+    id: "6",
+    name: "Herr's Carolina Reaper Chips",
+    image: "https://www.jerryamerica.com/cdn/shop/products/HERR_S-CAROLINA-REAPER-FLAVOURED-POTATO-CHIP-29-gr_360x.jpg?v=1572291552",
+    price: "4,99 €",
+    description: "Patatine ancora più piccanti con l'aggiunta del Carolina Reaper. ",
+
+},
+{
+    id: "7",
+    name: "Fanta ananas",
+    image: "https://www.jerryamerica.com/cdn/shop/products/1e01e4e0-ffc4-4fa6-8671-84fd98702e90_360x.jpg?v=1600294045",
+    price: "3,99 €",
+    description: "Fanta all'ananas, figurati se qualcuno lo compra ",
+
+},
+{
+    id: "8",
+    name: "Reeses",
+    image: "https://www.jerryamerica.com/cdn/shop/products/c6b471a5-a330-42a8-9c81-c38fc3f9f578_360x.jpg?v=1571688232",
+    price: "2,99 €",
+    description: "Crema spalmabile Reese's",
+
+},{
+    id: "9",
+    name: "Coca Cola Vanilla",
+    image: "https://www.jerryamerica.com/cdn/shop/products/coca-cola-vanilla-355ml_360x.jpg?v=1655153874",
+    price: "2,99 €",
+    description: "Coca Cola alla vaniglia. ",
 
 },
 ];
